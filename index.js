105f2c3983cb74c69723c922f306d7d5e4eed681
priscilla <pdjarbeng@outlook.com>
2021-10-16 15:52:54 -0400

    commit tree 270e2868ffce77a80a0a13759c06cef974d4e98e

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+
+function findMatching(array, stringz) {
+    const matching = array.filter(keyName => keyName.toLowerCase() === stringz.toLowerCase())
+    return matching
+}
+
+function fuzzyMatch(array, stringz) {
+    const match = array.filter(letter => letter.substring(0, 2) === stringz.substring(0, 2))
+    return match
+}
+
+function matchName(array, stringz) {
+    const result = array.filter(key => key.name === stringz)
+    return result
+}
